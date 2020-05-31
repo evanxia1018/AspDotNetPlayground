fe77515f88353086d4154adcc60803659777c8d1
Evan Xia <zhexi@microsoft.com>
2020-05-31 16:39:01 -0700

    Add project files.

---
+++ AspDotNetPlayground/Playground/DoAppDomainsShareTheSameThreadPool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspDotNetPlayground.Playground
+{
+    public class DoAppDomainsShareTheSameThreadPool
+    {
+        public static void Run()
+        {
+            var domain0 = AppDomain.CreateDomain("domain 0", null, AppDomain.CurrentDomain.SetupInformation);
+            var domain1 = AppDomain.CreateDomain("domain 1", null, AppDomain.CurrentDomain.SetupInformation);
+
+            Task.Run(() => {
+                domain0.DoCallBack(new CrossAppDomainDelegate(PrintDomainNameLoop));
+            });
+            Task.Run(() => {
+                domain1.DoCallBack(new CrossAppDomainDelegate(PrintDomainNameLoop));
+            });
+
+            Thread.Sleep(5000);
+            ThreadPool.GetMaxThreads(out int maxWorkerThreadCount, out _);
+            ThreadPool.GetAvailableThreads(out int availableWorkerThreadCount, out _);
+            int busyWorkerThreadCount = maxWorkerThreadCount - availableWorkerThreadCount;
+            Console.WriteLine("There are [{0}] busy worker threads", busyWorkerThreadCount);
+        }
+
+        private static void PrintDomainNameLoop()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                System.Diagnostics.Debug.WriteLine("I'm an AppDomain. My name is {0}", AppDomain.CurrentDomain.FriendlyName);
+            }
+        }
+    }
+}
---
+++ AspDotNetPlayground/Site.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspDotNetPlayground
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
